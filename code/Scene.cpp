394a4f8b749d74fadf672d8944d28c8063527727
welann <wzcing@outlook.com>
2023-06-10 17:48:17 +0800

    初步实现了碰撞

--- code/Scene.cpp
+++ code/Scene.cpp
@@ -57,11 +57,25 @@ void Scene::Initialize()
     m_bodies.push_back(body);
 
     // Add a ” ground ”spherethat won ’ tf a l lunder theinfluenceofg r a v i t y
-    body.m_position = Vec3(0, 0, -100);
+    body.m_position = Vec3(0, 0, -1000);
     body.m_orientation = Quat(0, 0, 0, 1);
     body.m_invMass = 0.0f;
-    body.m_shape = new ShapeSphere(100.0f);
+    body.m_shape = new ShapeSphere(1000.0f);
     m_bodies.push_back(body);
+}
+
+bool Intersect(const Body* bodyA, const Body* bodyB)
+{
+    const Vec3 ab = bodyB->m_position - bodyA->m_position;
+    const ShapeSphere* sphereA = (const ShapeSphere*)bodyA->m_shape;
+    const ShapeSphere* sphereB = (const ShapeSphere*)bodyB->m_shape;
+
+    const float radiusAB = sphereA->m_radius + sphereB->m_radius;
+    const float lengthSquare = ab.GetLengthSqr();
+    if (lengthSquare <= (radiusAB * radiusAB)) {
+        return true;
+    }
+    return false;
 }
 
 /*
@@ -80,6 +94,21 @@ void Scene::Update(const float dt_sec)
         Vec3 impulseGravity = Vec3(0, 0, -10) * mass * dt_sec;
         body->ApplyImpulseLinear(impulseGravity);
     }
+
+    for (int i = 0; i < m_bodies.size(); i++) {
+        for (int j = i + 1; j < m_bodies.size(); j++) {
+            Body* bodyA = &m_bodies[i];
+            Body* bodyB = &m_bodies[j];
+            if (0.0f == bodyA->m_invMass && 0.0f == bodyB->m_invMass) {
+                continue;
+            }
+            if (Intersect(bodyA, bodyB)) {
+                bodyA->m_linearVelocity.Zero();
+                bodyB->m_linearVelocity.Zero();
+            }
+        }
+    }
+
     for (int i = 0; i < m_bodies.size(); i++) {
         // Positionupdate
         m_bodies[i].m_position += m_bodies[i].m_linearVelocity * dt_sec;
--- code/application.h
+++ code/application.h
@@ -2,7 +2,6 @@
 //  application.h
 //
 #pragma once
-#include <corecrt_wstdio.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <stdio.h>
